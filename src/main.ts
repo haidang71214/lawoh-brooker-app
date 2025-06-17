c36f0445fed570e396051501327a8659795897dd
haidang71214 <haidang71214@gmail.com>
2025-06-17 14:45:18 +0700

    'update'

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
     // Cấu hình CORS
     app.enableCors({
-      origin: ['http://localhost:3000', process.env.NODE_ENV === 'production' ? 'https://lawohbe.onrender.com' : '*'],
+      origin: ['http://localhost:3000', process.env.NODE_ENV === 'production' ? 'https://yourdomain.com' : '*'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     });
@@ -33,7 +33,7 @@ async function bootstrap() {
     const swagger = SwaggerModule.createDocument(app, configSwagger);
     SwaggerModule.setup('Swagger', app, swagger);
 
-    // Lấy cổng từ process.env.PORT (Render) hoặc ConfigService
+    // Lấy port từ process.env.PORT (Render) hoặc ConfigService
     const port = process.env.PORT || configService.get<number>('PORT') || 8080;
 
     // Khởi tạo và setup Socket.io
@@ -41,13 +41,13 @@ async function bootstrap() {
     const commentService = app.get(CommentService);
     setupSocketIo(app, chatService, commentService);
 
-    // Khởi động ứng dụng
-    await app.listen(port);
-    console.log(`Ứng dụng đang chạy trên: http://localhost:${port}`);
-    console.log(`MongoDB URI: ${configService.get<string>('MONGODB_URI')}`);
+    // Khởi động ứng dụng trên 0.0.0.0
+    await app.listen(port, '0.0.0.0');
+    // render đẩy lên 0 0 0 0 á
+    console.log(`Ứng dụng đang chạy trên port: http://0.0.0.0:${port}`);
   } catch (error) {
-    console.error('Ứng dụng không khởi động được:', error.stack); // Log chi tiết lỗi
-    process.exit(1); // Thoát với mã lỗi để Render nhận biết
+    console.error('Ứng dụng không khởi động được:', error.stack);
+    process.exit(1);
   }
 }
 
