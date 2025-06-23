c2f513a34289aed402220d453de765094d284f00
haidang71214 <haidang71214@gmail.com>
2025-06-23 20:44:01 +0700

    'update'

--- src/config/socket-io.ts
+++ src/config/socket-io.ts
@@ -7,27 +7,66 @@ export function setupSocketIo(app: INestApplication, chatService: ChatService) {
   const server = app.getHttpServer();
   const io = new Server(server, { cors: { origin: '*' } });
 
+  // chỉ connection 1 lần
   io.on('connection', socket => {
     console.log('Client connected:', socket.id);
-
     socket.on('joinRoom', (roomId) => {
       socket.join(roomId);
       console.log(`Socket ${socket.id} joined room ${roomId}`);
     });
+    // check user online
+    socket.on('register',(clientId)=>{
+      // nó sẽ checkUser online và gửi tới dashboard
+      io.to('dashboard').emit('user-online', { clientId, isOnline: true });
+    })
 
     socket.on('sendMessage', async ({ conversationId, senderId, content }) => {
       const message = await chatService.addMessage(conversationId, senderId, content);
       io.to(conversationId).emit('newMessage', message);
     });
+// join dasboard ni hắn sẽ hứng trên fe về để làm cái join 
+    socket.on('join-dashboard', () => {
+      socket.join("dashboard"); // room
+      socket.emit("dashboard-joined");
+    })
 
+    socket.on('join-video-room', (roomId, clientId, requestClientId) => {
+      socket.join(roomId);
+      const room = io.sockets.adapter.rooms.get(roomId);
+      const numClients = room ? room.size : 0;
+      console.log(numClients);
+      console.log(roomId, clientId);
+// lỗi ở chỗ này, nó numClient khi rejcet là là null hoặc 1, nhưng khi khởi tạo lại thì nó đang auto = 2 
+      if (numClients === 1) {
+        io.to('dashboard').emit('room-update', { roomId, status: 'waiting', clients: [clientId, requestClientId] });
+      } else if (numClients === 2) {
+// check lại trạng thái stated vì nó làm hư watting
+        io.to('dashboard').emit('room-update', { roomId, status: 'started', clients: [requestClientId, clientId] });
+      }
+    });
+
+    socket.on('send-peer-id', ({ roomId, peerId, recipientId }) => {
+      socket.to(roomId).emit('receive-peer-id', { peerId });
+    });
+
+    socket.on('reject-call', (roomId) => {
+      io.to(roomId).emit('call-rejected', { message: 'Cuộc gọi đã bị hủy' });
+      const room = io.sockets.adapter.rooms.get(roomId);
+      if (room) {
+        for (const socketId of room) {
+          const socketInRoom = io.sockets.sockets.get(socketId);
+          if (socketInRoom) {
+            socketInRoom.leave(roomId);
+          }
+        }
+      }
+      io.to('dashboard').emit('room-update', { roomId, status: 'rejected', clients: [] });
+    });
+
+    // disconected
     socket.on('disconnect', () => {
       console.log('Client disconnected:', socket.id);
     });
-  // mỗi lần join 1 video là join 1 cái room với id chính là cái video đó
-  // mỗi lần out là disconnent
-  // on là send comment 
-  
   });
-  // làm thêm 1 cái comment
-  // mỗi lần block vô 
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
       //  deploy fe, thay đổi những thứ có ở cái localhost
       // gán cái fe vào đây
       // ,'http://localhost:3000'
-      origin: [process.env.NODE_ENV === 'production' ? `${URL_PRODUCTION}` : '*'],
+      origin: [process.env.NODE_ENV === 'production' ? `${URL_PRODUCTION}` : '*','http://localhost:3000'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     });
@@ -47,7 +47,7 @@ async function bootstrap() {
 
     // Khởi động ứng dụng trên 0.0.0.0 kệ mẹ cái env vì env đéo chạy được
     //process.env.PORT ||
-    const port =  10000; // Mặc định 10000 theo tài liệu Render
+    const port =  8080; // Mặc định 10000 theo tài liệu Render
 await app.listen(port, '0.0.0.0');
 console.log(`Ứng dụng đang chạy trên port: http://0.0.0.0:${port}`);
   } catch (error) {
